e144ffddd7bfb05264b509c7dc5d2ed2e9830e95
Pedro H. Alarcon <pedrohenriquealarcon@gmail.com>
2023-11-25 21:38:44 -0300

    Criado tela de edição da pessoa referente ao evento - 2023.11.25.5

--- src/pages/Hospedes/hospedes.jsx
+++ src/pages/Hospedes/hospedes.jsx
@@ -15,12 +15,23 @@ function Hospedes() {
     const [hospedesData, setHospedesData] = useState([]);
 
     const [carregando, setCarregando] = useState(false);
+
     const [abrirCadastro, setAbrirCadastro] = useState(false);
+    const [abrirEditar, setAbrirEditar] = useState(false);
+    const [abrirExcluir, setAbrirExcluir] = useState(false);
+
     const [pagante, setPagante] = useState(false);
     const [cobrante, setCobrante] = useState(false);
 
     const [nome, setNome] = useState('');
     const [comunidade, setComunidade] = useState('');
+    const [hospede, setHospede] = useState({});
+
+    const hoespedeInicial = {
+        pesCodigo: 0,
+        pagante: true,
+        cobrante: true,
+    }
 
     const getHospedes = async () => {
         setCarregando(true);
@@ -35,11 +46,42 @@ function Hospedes() {
         setAbrirCadastro(!abrirCadastro);
     }
 
+    const abrirFecharEditar = () => {
+        setAbrirEditar(!abrirEditar);
+    }
+
+    const abrirFecharExcluir = () => {
+        setAbrirExcluir(!abrirExcluir);
+    }
+
+    const atualizarHospede = async (event) => {
+        event.preventDefault();
+        setCarregando(true);
+        let retorno = await apiHospedes.putHospede(evento, nome, comunidade, pagante, cobrante);
+        if (retorno !== null) {
+            setHospedesData(retorno);
+        }
+        setCarregando(false);
+    }
+
     const handleCheck = (event) => {
         if (event.target.name === "pesCatequista")
             setPagante(event.target.checked);
         else if (event.target.name === "pesSalmista")
             setCobrante(event.target.checked);
+    }
+
+    const selecionaHospede = (hospede, operacao) => {
+        // const { name, value } = event.target;
+        // setHospede({
+        //     ...hospede,
+        //     [name]: value
+        // });
+        setHospede(hospede);
+        if (operacao === "Editar")
+            abrirFecharEditar();
+        else if (operacao === "Excluir")
+            abrirFecharExcluir();
     }
 
     const handleNome = (event) => {
@@ -52,6 +94,7 @@ function Hospedes() {
 
     useEffect(() => {
         getHospedes();
+        setHospede(hoespedeInicial);
     }, []);
 
     return (
@@ -86,10 +129,10 @@ function Hospedes() {
                                 <input className="form-check-input" type="checkbox" id="cobrante" value="cobrante" checked={hospede.cobrante} />
                             </td>
                             <td>
-                                <button className="btn btn-warning" onClick={() => this.selecionarEvento(hospede, "Editar")}>
+                                <button className="btn btn-warning" onClick={() => selecionaHospede(hospede, "Editar")}>
                                     <i className="fa fa-pencil"></i>
                                 </button>{" "}
-                                <button className="btn btn-danger" onClick={() => this.selecionarEvento(hospede, "Excluir")}>
+                                <button className="btn btn-danger" onClick={() => selecionaHospede(hospede, "Excluir")}>
                                     <i className="fa fa-trash"></i>
                                 </button>
                             </td>
@@ -97,112 +140,34 @@ function Hospedes() {
                     ))}
                 </tbody>
             </FormModel>
+            <FormEditar
+                nome={"Hóspede"}
+                abrir={abrirEditar}
+                funcAbrir={abrirFecharEditar}
+                funcPut={atualizarHospede}
+            >
+                <div className='row col-md-12 d-flex justify-content-around'>
+                    <div className='col-md-6'>
+                        <div className="form-check">
+                            <input className="form-check-input" type="checkbox" id="pagante" name="pesCatequista" onChange={handleCheck} />
+                            <label className="form-check-label p-1" htmlFor="pagante">
+                                Pagante
+                            </label>
+                        </div>
+                    </div>
+                    <div className='col-md-6'>
+                        <div className="form-check">
+                            <input className="form-check-input" type="checkbox" id="cobrante" name="pesSalmista" onChange={handleCheck} />
+                            <label className="form-check-label p-1" htmlFor="cobrante">
+                                Cobrante
+                            </label>
+                        </div>
+                    </div>
+                </div>
+
+            </FormEditar>
         </React.Fragment>
     );
 }
 
 export default Hospedes;
-
-
-// import React from 'react';
-
-// import FormModel from "../../forms/Modelo"
-// import FormInserir from "../../forms/FormInserir"
-// import FormEditar from "../../forms/FormEditar"
-// import FormExcluir from "../../forms/FormExcluir"
-
-// import { apiHospedes } from "../../services/Api";
-// import { useLocation } from 'react-router-dom'
-
-// class Hospedes extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             hospedesData: [],
-//             carregando: false,
-//             abrirCadastro: false,
-//         };
-//     }
-
-//     getHospedes = async () => {
-//         this.setState({ carregando: true });
-//         let retorno = await apiHospedes.getHospedes(this.props.evento);
-//         if (retorno.statusCode === 200)
-//             this.setState({ hospedesData: retorno.data, carregando: false });
-//         else
-//             this.setState({ carregando: false });
-//     }
-
-//     // getEventoCodigo = async () => {
-//     //     function useQuery() {
-//     //         const location = useLocation()
-//     //         const { from } = location.state
-//     //         return from;
-//     //     }
-
-//     //     return useQuery();
-//     // }
-
-//     abrirFecharCadastro = () => {
-//         this.setState({ abrirCadastro: !this.state.abrirCadastro });
-//     }
-
-//     componentDidMount() {
-//         this.setState({ carregando: true });
-//         this.getHospedes();
-//     }
-
-//     render() {
-//         return (
-//             <React.Fragment>
-//                 <FormModel
-//                     titulo="Listagem de Hóspedes"
-//                     subtitulo="CCMZ"
-//                     icone="user"
-//                     tipoContainer="form-container"
-//                     Cabecalho="Hóspedes"
-//                     BotaoAdd="Adicionar Hóspede"
-//                     getDados={this.getEventos}
-//                     getByNome={this.getVendedorNome}
-//                     funcAbrirCadastro={this.abrirFecharCadastro}
-//                     carregando={this.state.carregando}
-//                     colunas={[
-//                         { nome: "Nome" },
-//                         { nome: "Comunidade" },
-//                         { nome: "Pagante" },
-//                         { nome: "Cobrante" },
-//                     ]}
-//                 >
-//                     <tbody>
-//                         {this.state.hospedesData.map((hospede) => (
-//                             <tr key={hospede.pesCodigo}>
-//                                 <td className="pt-3">{hospede.pesNome}</td>
-//                                 <td className="pt-3"></td>
-//                                 <div className="col-md-3">
-//                                     <div className="form-check form-check-inline">
-//                                         <input className="form-check-input" type="checkbox" id="pesCatequista" value="pesCatequista" name="pesCatequista" onChange={this.handleCheck} />
-//                                         <label className="form-check-label" for="pesCatequista">Pagante</label>
-//                                     </div>
-//                                     <div className="form-check form-check-inline">
-//                                         <input className="form-check-input" type="checkbox" id="pesSalmista" value="pesSalmista" name="pesSalmista" onChange={this.handleCheck} />
-//                                         <label className="form-check-label" for="pesSalmista">Cobrante</label>
-//                                     </div>
-//                                 </div>
-//                                 <td>
-//                                     <button className="btn btn-warning" onClick={() => this.selecionarEvento(hospede, "Editar")}>
-//                                         <i className="fa fa-pencil"></i>
-//                                     </button>{" "}
-//                                     <button className="btn btn-danger" onClick={() => this.selecionarEvento(hospede, "Excluir")}>
-//                                         <i className="fa fa-trash"></i>
-//                                     </button>
-//                                 </td>
-//                             </tr>
-//                         ))}
-//                     </tbody>
-//                 </FormModel>
-//             </React.Fragment>
-//         );
-//     }
-// }
-
-// export default Hospedes;
